1766423e6d5dec7f3bd4117d1123722e5a6dc936
Asam Afzal <aainvests@outlook.com>
2022-09-29 08:30:30 -0400

    Setup components and routing

---
+++ src/Cart.js
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Cart = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>Hello from Profile</h1>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Cart;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <footer>
+            <nav>
+                <Link to="/">Home</Link> |{" "}
+                <Link to="/cart">Cart</Link>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer;
